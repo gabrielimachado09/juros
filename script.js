361893a476556fb07c2b9a63ea538589666127fb
gabrielimachado09 <gabrieli.machado.fernandes@escola.pr.gov.br>
2025-07-31 12:00:44 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 function calcular(){
 
-let capital= perseInt (document.getElementById('capital').value);
-let taxa = perseInt (document.getElementById('taxa').value);
-let tempo = perseInt (document.getElementById('tempo').value);
+let capital_inicial = parseInt(document.getElementById('capital_inicial').value);
+    let taxa = parseInt(document.getElementById('taxa').value);
+    let tempo = parseInt(document.getElementById('tempo').value);
 
-let porcentagem_de_juros = taxa/100;
-let juros = capital * porcentagem_de_juros * tempo;
-let montante = juros + capital;
-DocumentTimeline.getElementById('resultado').textContent ='resultado:' + montante;
+    let porcentagem_de_juro = taxa/100;
+    let juros = capital_inicial * porcentagem_de_juro * tempo;
+    let montante = juros + capital_inicial;
 
-
-
-
+    document.getElementById('Resultado_juros').textContent = "Juros: " + juros;
+    document.getElementById('Resultado_montante').textContent = "Montante: " + montante;
 
 }
